22cc0fbe2ac628bc7eb4d9d42135a994a9add697
keno-1337 <marell18625@stud.noroff.no>
2023-02-25 20:11:38 +0100

    3245345

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const button = document.getElementById('submitButton');
-button.addEventListener('click', function () {
-    fetch('https://official-joke-api.appspot.com/random_joke')
-    .then(function (result) {
-        console.log(result);
-    })
-    .catch(function (err) {
-        console.error(err);
-    });
-});
+
+  const jokeButton = document.getElementById("jokeButton");
+  const loadingIndicator = document.getElementById("loadingIndicator");
+
+  jokeButton.addEventListener("click", async () => {
+    loadingIndicator.style.display = "block"; // Show the loading indicator
+
+    const url = "https://official-joke-api.appspot.com/random_joke";
+    const response = await fetch(url);
+    const data = await response.json();
+    const setup = data.setup;
+    const punchline = data.punchline;
+    const jokeDiv = document.getElementById("joke");
+    jokeDiv.innerHTML = `<p>${setup}</p><p>${punchline}</p>`;
+
+    loadingIndicator.style.display = "none"; // Hide the loading indicator again
+  });
+
