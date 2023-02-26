977001ba4e9b4083b8e0eb529cba3bba2ab72b76
keno-1337 <marell18625@stud.noroff.no>
2023-02-26 19:54:02 +0100

    aaaaaaa

---
+++ contactValidation.js
@@ -0,0 +1,60 @@
+const form = document.getElementById("contact-form");
+const nameInput = document.getElementById("name");
+const subjectInput = document.getElementById("subject");
+const emailInput = document.getElementById("email");
+const addressInput = document.getElementById("address");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  validateForm();
+});
+
+function validateForm() {
+  const nameValue = nameInput.value.trim();
+  const subjectValue = subjectInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const addressValue = addressInput.value.trim();
+
+  if (nameValue === "") {
+    displayError("name", "Name is required.");
+  } else {
+    hideError("name");
+  }
+
+  if (subjectValue.length < 10) {
+    displayError("subject", "Subject must have a minimum length of 10 characters.");
+  } else {
+    hideError("subject");
+  }
+
+  if (!isValidEmail(emailValue)) {
+    displayError("email", "Email must be formatted like an email address.");
+  } else {
+    hideError("email");
+  }
+
+  if (addressValue.length < 25) {
+    displayError("address", "Address must have a minimum length of 25 characters.");
+  } else {
+    hideError("address");
+  }
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function displayError(inputName, message) {
+  const inputElement = document.getElementById(inputName);
+  const errorElement = document.getElementById(`${inputName}-error`);
+  inputElement.classList.add("invalid");
+  errorElement.textContent = message;
+}
+
+function hideError(inputName) {
+  const inputElement = document.getElementById(inputName);
+  const errorElement = document.getElementById(`${inputName}-error`);
+  inputElement.classList.remove("invalid");
+  errorElement.textContent = "";
+}
--- script.js
+++ script.js
@@ -1,10 +1,62 @@
+// attempt at details.html
+//I can not get this part to work properly... No idea what im doing wrong.
+// fetch('https://official-joke-api.appspot.com/random_joke')
+//   .then(response => response.json())
+//   .then(data => {
 
+//     const container = document.querySelector('#results-container');
+//     data.forEach(result => {
+//       const resultHtml = `
+//         <div class="result">
+//           <h2>${result.type}</h2>
+//           <p>${result.setup}</p>
+//           <p>${result.punchline}</p>
+//           <p>${result.id}</p>
+//           <a href="details.html?id=${result.id}">View Details</a>
+//         </div>
+//       `;
+//       container.insertAdjacentHTML('beforeend', resultHtml);
+//     });
+//   })
+//  .catch(error => {
+//    const errorMessage = `<p>Sorry, there was an error loading the data.</p>`;
+//    container.insertAdjacentHTML('beforeend', errorMessage);
+//    console.error(error);
+//  });
+//  function newFunction() {
+//   return '#results-container';
+//  }
+
+function getJokeById() {
+    const jokeId = document.getElementById("jokeId").value;
+    const apiUrl = `https://official-joke-api.appspot.com/jokes/${jokeId}`;
+    
+    // loading
+    const jokeDiv = document.getElementById("joke");
+    jokeDiv.innerHTML = "<p>Clearing throat...</p>";
+    
+    // API Request
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Show joke
+        const setup = data.setup;
+        const punchline = data.punchline;
+        jokeDiv.innerHTML = `<p>${setup}</p><p><em>${punchline}</em></p>`;
+      })
+      .catch(error => {
+        // error
+        jokeDiv.innerHTML = "<p>Error: Unable to retrieve joke.</p>";
+      });
+  }
+  
   const jokeButton = document.getElementById("jokeButton");
+  const jokesButton = document.getElementById("jokesButton");
   const loadingIndicator = document.getElementById("loadingIndicator");
 
   jokeButton.addEventListener("click", async () => {
-    loadingIndicator.style.display = "block"; // Show the loading indicator
-
+    loadingIndicator.style.display = "block"; 
+    // loading
     const url = "https://official-joke-api.appspot.com/random_joke";
     const response = await fetch(url);
     const data = await response.json();
@@ -13,6 +65,19 @@
     const jokeDiv = document.getElementById("joke");
     jokeDiv.innerHTML = `<p>${setup}</p><p>${punchline}</p>`;
 
-    loadingIndicator.style.display = "none"; // Hide the loading indicator again
+    loadingIndicator.style.display = "none"; // Hide the loading indicator
+  });
+
+  jokesButton.addEventListener("click", async () => {
+    loadingIndicator.style.display = "block"; // Show the loading indicator
+
+    const url = "https://official-joke-api.appspot.com/random_ten";
+    const response = await fetch(url);
+    const data = await response.json();
+    const jokes = data.map((joke) => `<p>${joke.setup} ${joke.punchline}</p>`);
+    const jokeDiv = document.getElementById("joke");
+    jokeDiv.innerHTML = jokes.join("");
+
+    loadingIndicator.style.display = "none"; // Hide the loading indicator
   });
 
